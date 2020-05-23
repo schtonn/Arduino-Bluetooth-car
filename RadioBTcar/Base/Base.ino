143cf59f1686f3331d300f167d1081004f55c28d
schtonn <m18519511495@163.com>
2020-05-23 22:51:00 +0800

    none ping

--- RadioBTcar/Base/Base.ino
+++ RadioBTcar/Base/Base.ino
@@ -1,9 +1,51 @@
+/*
+   Radio Bluetooth Car
+   Bluetooth -> Radio Part
+   Written By Alex
+   2020/23/05
+*/
+
+#include <SoftwareSerial.h>
+
+const byte RADIOs = 2;
+const byte RADIOr = 3;
+SoftwareSerial RADIO(RADIOs, RADIOr);
+const byte BTs = 4;
+const byte BTr = 5;
+SoftwareSerial BT(BTs, BTr);
+
+void blink(){
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+}
+
 void setup() {
   // put your setup code here, to run once:
 
+  pinMode(13, OUTPUT);
+
+  pinMode(RADIOs, OUTPUT);
+  pinMode(RADIOr, INPUT);
+  
+  pinMode(BTs, OUTPUT);
+  pinMode(BTr, INPUT);
+
+  RADIO.begin(9600);
+  BT.begin(9600);
+  Serial.begin(9600);
+
+  char code;
+
+  delay(3000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (BT.available()) {
+    code = RADIO.read();
+    Serial.println(code);
+    RADIO.println(code);
+    blink();
+  }
 }
--- RadioBTcar/Top/Top.ino
+++ RadioBTcar/Top/Top.ino
@@ -7,9 +7,9 @@
 
 #include <SoftwareSerial.h>
 
-const byte BTsend = 11;
-const byte BTrec = 12;
-SoftwareSerial  BT(BTrec,BTsend);
+const byte RADIOs = 10;
+const byte RADIOr = 11;
+SoftwareSerial RADIO(RADIOs, RADIOr);
 
 const byte LF = 2; //Left Forward +
 const byte LB = 3; //Left Back +
@@ -18,43 +18,79 @@ const byte RB = 5; //Righy Back +
 const byte LS = 6; //Left Speed
 const byte RS = 7; //Right Speed
 
-void WRITE(int s1,int s2,bool a,bool b,bool c,bool d){
-  analogWrite(LS,s1);
-  analogWrite(RS,s2);
-  if(a)digitalWrite(LF,HIGH);
-  else digitalWrite(LF,LOW);
-  
-  if(b)digitalWrite(LB,HIGH);
-  else digitalWrite(LB,LOW);
-  
-  if(c)digitalWrite(RF,HIGH);
-  else digitalWrite(RF,LOW);
-  
-  if(d)digitalWrite(RB,HIGH);
-  else digitalWrite(RB,LOW);
+void WRITE(int s1, int s2, bool a, bool b, bool c, bool d) {
+  analogWrite(LS, s1);
+  analogWrite(RS, s2);
+
+  if (a)digitalWrite(LF, HIGH);
+  else digitalWrite(LF, LOW);
+
+  if (b)digitalWrite(LB, HIGH);
+  else digitalWrite(LB, LOW);
+
+  if (c)digitalWrite(RF, HIGH);
+  else digitalWrite(RF, LOW);
+
+  if (d)digitalWrite(RB, HIGH);
+  else digitalWrite(RB, LOW);
 }
-void S(){
-  WRITE(0,0,false,false,false,false);
+void S() {
+  WRITE(0, 0, false, false, false, false);
 }
-void F(int SPEED){
-  WRITE(SPEED,SPEED,true,false,true,false);
+void F(int SPEED) {
+  WRITE(SPEED, SPEED, true, false, true, false);
 }
-void B(int SPEED){
-  WRITE(SPEED,SPEED,false,true,false,true);
+void B(int SPEED) {
+  WRITE(SPEED, SPEED, false, true, false, true);
 }
-void L(int SPEED){
-  WRITE(SPEED,SPEED,true,false,false,true);
+void L(int SPEED) {
+  WRITE(SPEED, SPEED, true, false, false, true);
 }
-void R(int SPEED){
-  WRITE(SPEED,SPEED,false,true,true,false);
+void R(int SPEED) {
+  WRITE(SPEED, SPEED, false, true, true, false);
 }
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LF, OUTPUT);
+  pinMode(LB, OUTPUT);
+  pinMode(RF, OUTPUT);
+  pinMode(RB, OUTPUT);
+  pinMode(LS, OUTPUT);
+  pinMode(RS, OUTPUT);
 
+  pinMode(RADIOs, OUTPUT);
+  pinMode(RADIOr, INPUT);
+
+
+  RADIO.begin(9600);
+  Serial.begin(9600);
+
+  char code;
+
+  delay(3000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (RADIO.available()) {
+    code = RADIO.read();
+    Serial.println(code);
+    switch (code) {
+      case 'f':
+        F();
+        break;
+      case 'b':
+        B();
+        break;
+      case 'l':
+        L();
+        break;
+      case 'r':
+        R();
+        break;
+      default:
+        S();
+    }
+  }
 }
